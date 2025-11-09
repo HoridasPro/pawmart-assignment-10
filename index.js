875967df3bc6d9e0362305ba7b6990b70bc609f0
HoridasPro <horidassarker6@gmail.com>
2025-11-10 00:49:08 +0600

    added to post, patch and delete

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
 const express = require("express");
 const cors = require("cors");
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.JSON || 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.mulyrzf.mongodb.net/?appName=Cluster0";
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+
+// pawMartProject
+// cD2bkwf99qc6NZk0
+const uri =
+  "mongodb+srv://pawMartProject:cD2bkwf99qc6NZk0@cluster0.mulyrzf.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("hello welcome to my project");
 });
+
+async function run() {
+  try {
+    await client.connect();
+    const pawMartProject = client.db("pawMartProject");
+    const productsCollection = pawMartProject.collection("products");
+
+    // For the post
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    // For the delete
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // For the patch
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+        },
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Port is runnign on this port ${port}`);
