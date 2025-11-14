a424195926e75973313c8001cca2db3309de6dd6
HoridasPro <horidassarker6@gmail.com>
2025-11-15 00:28:11 +0600

    final again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mulyrzf.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     const pawMartProject = client.db("pawMartProject");
     const productsCollection = pawMartProject.collection("products");
     const orderCollection = pawMartProject.collection("orders");
@@ -35,7 +35,7 @@ async function run() {
     // For the get only one data
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
+      const query = { _id: new  ObjectId(id) };
       const result = await productsCollection.findOne(query);
       res.send(result);
     });
@@ -160,7 +160,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
